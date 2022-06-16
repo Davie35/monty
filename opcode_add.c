659c88399eea5a9803cf2f930c003803555c27b8
Davie35 <dnjoroge35@gmail.com>
2022-06-16 18:27:17 +0300

    monty

---
+++ opcode_add.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+  * addop - finds sum of top two elements
+  * @stack: pointer to the head node pointer of stack
+  * @nline: line number
+  *
+  * Return: NAIN
+  */
+	void addop(stack_t **stack, unsigned int nline)
+	{
+	stack_t *temp;
+
+	if (!(*stack) || !(*stack)->next)
+	{
+	fprintf(stderr, "L%d: can't add, stack too short\n", nline);
+	exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	(*stack)->next->n += (*stack)->n;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
+	}
